97b2e99458a96ead79c00f8273b78c0801836da6
Muhammed Konulga <konulga0@gmail.com>
2024-07-19 21:37:17 +0300

    Many to Many denendi düzenlenilecek

--- library/admin.py
+++ library/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
-from .models import Article, Reporter
+from .models import Article, Reporter, Language
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ("headline", "reporter", "pub_date", "slug")
+    list_display = ( "headline","id", "reporter", "pub_date", "slug", )
     search_fields = ("headline", "content")
     readonly_fields = ("pub_date", "reporter")
+    list_filter = ("reporter",)
 
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Reporter)
+admin.site.register(Language)
--- library/models.py
+++ library/models.py
@@ -14,13 +14,23 @@ class Reporter(models.Model):
         self.slug = slugify(self.full_name)
         super().save(args, **kwargs)
 
-    
+class Language(models.Model):
+    lang_sc = models.CharField(max_length=7)
+    lang_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.lang_sc
+
+
+
+
 class Article(models.Model):
     pub_date = models.DateField()
     headline = models.CharField(max_length=200)
     content = models.TextField()
     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
     slug = models.SlugField(blank=True, unique=True, db_index=True, editable = False)
+    langs = models.ManyToManyField(Language)
 
     def __str__(self):
         return self.headline
@@ -28,3 +38,4 @@ class Article(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.headline)
         super().save(args, **kwargs)
+
